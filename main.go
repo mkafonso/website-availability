6e2f6ffa9f42a7c367ec629d376ccbbf4939f883
senor-dev <makaya2afonso@gmail.com>
2023-07-05 22:48:53 -0300

    save and read logs

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -37,7 +40,7 @@ func handleCommand(input int) {
 	case 1:
 		startMonitoring()
 	case 2:
-		fmt.Println("Two")
+		showLogs()
 	case 3:
 		os.Exit(0)
 	default:
@@ -56,10 +59,12 @@ func checkWebsiteStatus(site string) {
 
 	if response.StatusCode == http.StatusOK {
 		fmt.Printf("website: %-40v  StatusOK: %v \n", site, http.StatusOK)
+		handleLogFile(site, true)
 		return
 	}
 
 	fmt.Println("Status Not OK", response.StatusCode)
+	handleLogFile(site, false)
 }
 
 func startMonitoring() {
@@ -72,6 +77,18 @@ func startMonitoring() {
 
 		time.Sleep(DELAY_BETWEEN_MONITORING)
 	}
+}
+
+func showLogs() {
+	file, err := ioutil.ReadFile("logs.txt")
+
+	if err != nil {
+		fmt.Printf("Error opening logs.txt")
+	}
+
+	clearScreen()
+
+	fmt.Println(string(file))
 }
 
 func readFromFile() []string {
@@ -95,6 +112,34 @@ func readFromFile() []string {
 	}
 
 	return sites
+}
+
+func handleLogFile(site string, isAvailable bool) {
+	file, err := os.OpenFile("logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Printf("Error opening logs.txt")
+	}
+
+	date := time.Now().Format("01/02/2006 15:04:05PM")
+	log := fmt.Sprintf("%-25v website: %-40v  IsOnline: %v \n", date, site, isAvailable)
+
+	file.WriteString(log)
+
+	file.Close()
+}
+
+func clearScreen() {
+	var clearCmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		clearCmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		clearCmd = exec.Command("clear")
+	}
+
+	clearCmd.Stdout = os.Stdout
+	clearCmd.Run()
 }
 
 func main() {
